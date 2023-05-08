cba99c78b2c86dab9723dfe957abe0b3bf99775b
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-05-08 06:57:06 +0200

    joinable world

--- export_mod/init.lua
+++ export_mod/init.lua
@@ -11,20 +11,22 @@ dofile(MP .. "/map.lua")
 local mb_pos1 = vector.new(-3, -2, -3)
 local mb_pos2 = vector.new(3, 5, 3)
 
-minetest.register_on_mods_loaded(function()
-    minetest.after(1, function()
-        minetest.mkdir(mtwebview.basepath)
-        mtwebview.export_nodedefs()
-        mtwebview.export_textures()
+if minetest.settings:get_bool("export_mod_autoexport") then
+    minetest.register_on_mods_loaded(function()
+        minetest.after(1, function()
+            minetest.mkdir(mtwebview.basepath)
+            mtwebview.export_nodedefs()
+            mtwebview.export_textures()
 
-        local pos1 = vector.multiply(mb_pos1, 16)
-        local pos2 = vector.multiply(mb_pos2, 16)
-        minetest.emerge_area(pos1, pos2, function(_, _, calls_remaining)
-            if calls_remaining == 0 then
-                mtwebview.export_map(mb_pos1, mb_pos2)
-                minetest.request_shutdown("done")
-            end
+            local pos1 = vector.multiply(mb_pos1, 16)
+            local pos2 = vector.multiply(mb_pos2, 16)
+            minetest.emerge_area(pos1, pos2, function(_, _, calls_remaining)
+                if calls_remaining == 0 then
+                    mtwebview.export_map(mb_pos1, mb_pos2)
+                    minetest.request_shutdown("done")
+                end
+            end)
+
         end)
-
     end)
-end)
+end
