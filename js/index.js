1fb6cdc9efbf676092efad70224d662bee4e2911
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-03-20 11:16:30 +0100

    basics

---
+++ js/index.js
@@ -0,0 +1,3 @@
+import { Mesh } from '../node_modules/three/build/three.module.js';
+
+console.log(Mesh);
---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+
+export default [{
+	input: 'js/index.js',
+	output: {
+		file :'js/bundle.js',
+		format: 'iife',
+		sourcemap: true,
+		compact: true
+	}
+}];
