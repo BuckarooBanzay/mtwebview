dfa0a219843d4f5cdb38442c9dc8a17ecc9435a9
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-03-31 12:16:10 +0200

    nodeside

--- js/util/NodeSide.js
+++ js/util/NodeSide.js
@@ -1,18 +1,29 @@
 import { Matrix4 } from "three"
 import Pos from "./Pos.js"
 
-class NodeSide {
-    constructor(dir, rotationmatrix) {
-        this.dir = dir,
-        this.rotationmatrix = rotationmatrix
-    }
-}
-
 export default {
-    XP: new NodeSide(new Pos(1,0,0), new Matrix4().makeRotationY(-Math.PI/2)),
-    XN: new NodeSide(new Pos(-1,0,0), new Matrix4().makeRotationY(Math.PI/2)),
-    YP: new NodeSide(new Pos(0,1,0), new Matrix4().makeRotationX(-Math.PI/2)),
-    YN: new NodeSide(new Pos(0,-1,0), new Matrix4().makeRotationX(Math.PI/2)),
-    ZP: new NodeSide(new Pos(0,0,1), new Matrix4()),
-    ZN: new NodeSide(new Pos(0,0,-1), new Matrix4().makeRotationY(Math.PI))
+    XP: {
+        dir: new Pos(1,0,0),
+        rotationmatrix: new Matrix4().makeRotationY(-Math.PI/2)
+    },
+    XN: {
+        dir: new Pos(-1,0,0),
+        rotationmatrix: new Matrix4().makeRotationY(Math.PI/2)
+    },
+    YP: {
+        dir: new Pos(0,1,0),
+        rotationmatrix: new Matrix4().makeRotationX(-Math.PI/2)
+    },
+    YN: {
+        dir: new Pos(0,-1,0),
+        rotationmatrix: new Matrix4().makeRotationX(Math.PI/2)
+    },
+    ZP: {
+        dir: new Pos(0,0,1),
+        rotationmatrix: new Matrix4()
+    },
+    ZN: {
+        dir: new Pos(0,0,-1),
+        rotationmatrix: new Matrix4().makeRotationY(Math.PI)
+    }
 }
