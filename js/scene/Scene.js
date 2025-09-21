206373050d56ec77b729aae24bf86582e108e384
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-09-21 13:58:15 +0200

    first person control wip

--- js/scene/Scene.js
+++ js/scene/Scene.js
@@ -1,10 +1,11 @@
-import { Color, PerspectiveCamera, Scene, WebGLRenderer, AmbientLight } from "three";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Color, PerspectiveCamera, Scene, WebGLRenderer, AmbientLight, Vector3, Clock } from "three";
+import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls.js';
 
 export default class {
 
+    clock = new Clock()
     scene = new Scene()
-    camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
     active = true;
 
     constructor(el) {
@@ -13,24 +14,33 @@ export default class {
         this.renderer = new WebGLRenderer({ canvas: el })
         this.renderer.setSize(window.innerWidth, window.innerHeight)
 
-        this.controls = new OrbitControls(this.camera, el)
-        this.controls.listenToKeyEvents(document.body)
-        this.controls.minDistance = 5
-        this.controls.maxDistance = 500
+        this.camera.position.x = 30
+        this.camera.position.y = -30
+        this.camera.position.z = -30
+        this.camera.lookAt(new Vector3(0, 0, 0));
 
-        this.controls.target.x = 30
-        this.controls.target.y = -30
-        this.controls.target.z = -30
+        this.controls = new FirstPersonControls(this.camera, el)
+        this.controls.lookSpeed = 0.4;
+        this.controls.movementSpeed = 20;
+        this.controls.noFly = true;
+        this.controls.lookVertical = true;
+        this.controls.constrainVertical = true;
+        this.controls.verticalMin = 1.0;
+        this.controls.verticalMax = 2.0;
+        this.controls.lon = -150;
+        this.controls.lat = 120;
 
         this.scene.add(new AmbientLight(0xffffff));
 
-        this.controls.addEventListener( 'change', () => this.render() );
+        //this.controls.addEventListener( 'change', () => this.render() );
 
         this.animate()
     }
 
     animate() {
-        this.controls.update()
+        this.controls.update(this.clock.getDelta())
+        this.renderer.render(this.scene, this.camera)
+        console.log(`Calls: ${this.renderer.info.render.calls}, Triangles: ${this.renderer.info.render.triangles}`)
         window.requestAnimationFrame(() => this.animate())
     }
 
@@ -38,8 +48,6 @@ export default class {
         if (!this.active) {
             return
         }
-        this.renderer.render(this.scene, this.camera)
-        console.log(`Calls: ${this.renderer.info.render.calls}, Triangles: ${this.renderer.info.render.triangles}`)
     }
 
     addMesh(m) {
