4eb608d75be42153b14b340008b9eaa2e1faa3fa
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-09-23 13:11:38 +0200

    eslint update

--- js/mesh/drawtype/Nodebox.js
+++ js/mesh/drawtype/Nodebox.js
@@ -8,26 +8,26 @@ const sidelist = Object.keys(NodeSide)
 export default class extends Base {
 
     processNodebox(gh, box, pos, c, side, nodedef, node) {
-        let size_x, size_y, center_x, center_y, offset_y
+        let size_x, size_y, center_x, center_y//, offset_y
         if (side == NodeSide.YP) {
             size_x = box[3] - box[0]
             size_y = box[5] - box[2]
             center_x = (box[0] + box[3]) / 2
             center_y = (box[2] + box[5]) / 2
-            offset_y = box[4]
+            //offset_y = box[4]
         } else if (side == NodeSide.YN) {
             size_x = box[3] - box[0]
             size_y = box[5] - box[2]
             center_x = (box[0] + box[3]) / 2
             center_y = (box[2] + box[5]) / 2
-            offset_y = box[1]
+            //offset_y = box[1]
         } else if (side == NodeSide.ZN) {
             // wip
             size_x = box[3] - box[0]
             size_y = box[4] - box[1]
             center_x = (box[0] + box[3]) / 2
             center_y = (box[1] + box[4]) / 2
-            offset_y = box[2]
+            //offset_y = box[2]
         } else {
             return
         }
@@ -52,7 +52,7 @@ export default class extends Base {
         for (let i=0; i<sidelist.length; i++) {
             const sidename = sidelist[i]
             const side = NodeSide[sidename]
-            const neighbor_pos = pos.add(side.dir)
+            //const neighbor_pos = pos.add(side.dir)
 
             const texture_def = this.getTextureDef(nodedef, side)
             const material = await this.matmgr.createMaterial(texture_def, true, FrontSide, true)
