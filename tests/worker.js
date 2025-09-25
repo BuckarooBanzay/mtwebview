e52f9c28ea2dab8e8f47f4c84693d41f63424b7a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-09-25 07:36:40 +0200

    worker wip (module issues)

--- tests/worker.js
+++ tests/worker.js
@@ -1,2 +1,8 @@
+import * as THREE from '/node_modules/three/build/three.module.js';
 
-console.log(URL.createObjectURL)
+console.log(THREE)
+
+
+if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope) {
+    console.log("worker!")
+}
