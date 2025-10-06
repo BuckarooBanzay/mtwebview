666a2c9f30bc1f2699b75f829d855c6d4d671e03
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-10-06 14:38:42 +0200

    worker promise interface

--- js/worker/WebWorker.js
+++ js/worker/WebWorker.js
@@ -1,8 +1,31 @@
 
 export class WebWorker {
 
+    // id -> fn()
+    resolvers = {}
+
     constructor(scriptfile = "js/bundle.js") {
         this.worker = new Worker(scriptfile)
+        this.worker.onmessage = e => this.onMessage(e)
+    }
+
+    onMessage(e) {
+        const id = e.data.id
+        const resolver = this.resolvers[id]
+        if (resolver) {
+            resolver(e.data)
+            delete this.resolvers[id]
+        }
+    }
+
+    async postData(data) {
+        const id = parseInt(Math.random() * 100000)
+        data.id = id
+        const promise = new Promise(resolve => {
+            this.resolvers[id] = resolve
+        })
+        this.worker.postMessage(data)
+        return promise
     }
 
     terminate() {
@@ -10,10 +33,7 @@ export class WebWorker {
     }
 
     async init() {
-        this.worker.postMessage({
-            type: "init"
-        })
-        // TODO: promise, onmessage
+        return this.postData({ type: "init" })
     }
 
     async render_geometries() {
