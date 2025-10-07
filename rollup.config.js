f8863a9dc0fa133500e8afff7cadf30b6ec504b7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-10-07 08:24:25 +0200

    prod build

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 
+const isProduction = process.env.BUILD === 'production';
+
 export default [{
 	input: 'js/index.js',
-	plugins: [resolve(), terser()],
+	plugins: [
+		resolve(),
+		isProduction && terser()
+	],
 	output: {
 		file :'js/bundle.js',
 		format: 'iife',
